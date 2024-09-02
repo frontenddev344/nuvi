d9e994c3f759394af0749edf14e8a5801bcbe74b
nishant <nd@int.iqsay.com>
2024-09-02 13:00:56 +0530

    update header banner

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
     $(".menu-toggle").click(function(){
       $("body").addClass("toggle");
-    });
-  });
-
-  $(document).ready(function(){
-    $(".closed-menu").click(function(){
-      $("body").removeClass("toggle");
+      $(".closed-menu").click(function(){
+        $("body").removeClass("toggle");
+      });
     });
   });

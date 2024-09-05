82a06ff677000b526932b3bb08a9fb36ca9967ad
nishant <nd@int.iqsay.com>
2024-09-05 20:09:15 +0530

    updTE

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,24 @@ $(document).ready(function(){
       });
     });
   });
+
+
+  let calVar = document.querySelector(".Calendar-Variant");
+  let calContent = document.querySelector(".calendar-sec-content");
+  let stick = document.querySelector(".stick");
+  
+  calContent.addEventListener("mousemove", function(event) {
+    // Get mouse X position relative to the calendar content
+    let mouseX = event.clientX - calContent.getBoundingClientRect().left;
+  
+    // Move the stick to follow the mouse
+    stick.style.left = mouseX + "px";
+    // stick.style.style.transform =  mouseX + "px" / 10 + "px";
+    // Calculate opacity based on stick position
+    let calWidth = calContent.offsetWidth;
+    let opacity = mouseX / calWidth;
+  
+    // Set the opacity for the Calendar-Variant element
+    calVar.style.opacity = opacity;
+  });
+  

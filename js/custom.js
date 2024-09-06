456a59131a86f075e0124988acca974c29e163b3
nishant <nd@int.iqsay.com>
2024-09-06 12:02:53 +0530

    update calender animation

--- js/custom.js
+++ js/custom.js
@@ -8,22 +8,9 @@ $(document).ready(function(){
   });
 
 
-  let calVar = document.querySelector(".Calendar-Variant");
-  let calContent = document.querySelector(".calendar-sec-content");
-  let stick = document.querySelector(".stick");
+
   
-  calContent.addEventListener("mousemove", function(event) {
-    // Get mouse X position relative to the calendar content
-    let mouseX = event.clientX - calContent.getBoundingClientRect().left;
-  
-    // Move the stick to follow the mouse
-    stick.style.left = mouseX + "px";
-    // stick.style.style.transform =  mouseX + "px" / 10 + "px";
-    // Calculate opacity based on stick position
-    let calWidth = calContent.offsetWidth;
-    let opacity = mouseX / calWidth;
-  
-    // Set the opacity for the Calendar-Variant element
-    calVar.style.opacity = opacity;
-  });
-  
+  const CalContainer = document.querySelector('.calender-container');
+  document.querySelector('.slider').addEventListener('input', (e) => {
+    CalContainer.style.setProperty('--position', `${e.target.value}%`);
+  })

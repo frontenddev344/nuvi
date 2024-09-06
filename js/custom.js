716bd630100b47a068cbe04c0253cd65c53ff782
nishant <nd@int.iqsay.com>
2024-09-06 13:00:30 +0530

    update animation

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,24 @@ $(document).ready(function(){
 
 
   
+  // const CalContainer = document.querySelector('.calender-container');
+  // document.querySelector('.slider').addEventListener('input', (e) => {
+  //   CalContainer.style.setProperty('--position', `${e.target.value}%`);
+  // })
+
   const CalContainer = document.querySelector('.calender-container');
-  document.querySelector('.slider').addEventListener('input', (e) => {
-    CalContainer.style.setProperty('--position', `${e.target.value}%`);
-  })
+  const imageBefore = document.querySelector('.image-before');
+  const slider = document.querySelector('.slider');
+  
+  slider.addEventListener('input', (e) => {
+    const value = e.target.value;
+  
+    // Set the custom property for positioning
+    CalContainer.style.setProperty('--position', `${value}%`);
+  
+    // Map the slider value to opacity (0 to 1)
+    const opacityValue = value / 100;
+    imageBefore.style.opacity = opacityValue;
+  });
+  
+  
